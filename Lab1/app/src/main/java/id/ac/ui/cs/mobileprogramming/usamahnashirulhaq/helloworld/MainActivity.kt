7beb0a1b9cc429280144c03b6d4c11033544e4a5
Usamah Nashirul Haq <usamah.nashirul@ui.ac.id>
2020-10-02 18:43:13 +0700

    commit lab-3

--- Lab1/app/src/main/java/id/ac/ui/cs/mobileprogramming/usamahnashirulhaq/helloworld/MainActivity.kt
+++ Lab1/app/src/main/java/id/ac/ui/cs/mobileprogramming/usamahnashirulhaq/helloworld/MainActivity.kt
@@ -1,33 +1,87 @@
 package id.ac.ui.cs.mobileprogramming.usamahnashirulhaq.helloworld
 
+import android.content.Intent
+import android.graphics.Typeface
 import android.os.Bundle
+import android.util.Log
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.*
 import androidx.appcompat.app.AppCompatActivity
 import id.ac.ui.mobileprogramming.usamahnashirulhaq.helloworld.R
-import kotlinx.android.synthetic.main.activity_main.*
-import java.text.SimpleDateFormat
-import java.util.*
+import androidx.databinding.DataBindingUtil
+import id.ac.ui.mobileprogramming.usamahnashirulhaq.helloworld.databinding.ActivityMainBinding
+import kotlin.math.log
 
 class MainActivity : AppCompatActivity() {
+    private lateinit var binding: ActivityMainBinding
+    private lateinit var ivIntro: ImageView
+    private lateinit var tvIntroUpper: TextView
+    private lateinit var tvIntroLower: TextView
+    private lateinit var btnStarted: Button
+    private lateinit var btnExit: ImageButton
+    private lateinit var imageViewAnim: Animation
+    private lateinit var textViewAnim: Animation
+    private lateinit var btnGetStartedAnim: Animation
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        dateView.setText(getTime())
-        buttonCheckTime.setOnClickListener() {
-            updateTime()
+        if (getIntent().getBooleanExtra("EXIT", false)) {
+            finish();
         }
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_main)
+
+        ivIntro = binding.ivIntro
+        tvIntroUpper = binding.tvIntroUpper
+        tvIntroLower = binding.tvIntroLower
+        btnStarted = binding.btnStarted
+        btnExit = binding.btnexit
+
+        //load animations
+        imageViewAnim = AnimationUtils.loadAnimation(this, R.anim.image_view_anim)
+        textViewAnim = AnimationUtils.loadAnimation(this, R.anim.textview_anim)
+        btnGetStartedAnim = AnimationUtils.loadAnimation(this, R.anim.btn_getstarted_anim)
+
+        //implement animation
+        ivIntro.startAnimation(imageViewAnim)
+        tvIntroUpper.startAnimation(textViewAnim)
+        tvIntroLower.startAnimation(textViewAnim)
+        btnStarted.startAnimation(btnGetStartedAnim)
+
+        //button action
+        btnStarted.setOnClickListener(View.OnClickListener {
+            var intent: Intent = Intent(this, StopWatchAct::class.java)
+            intent.addFlags(Intent.FLAG_ACTIVITY_NO_ANIMATION)
+            startActivity(intent)
+            Log.i("MainActivity","Button Clicked")
+        })
+
+        btnExit.setOnClickListener(View.OnClickListener {
+            Log.i("MainActivity","Exit Button Clicked")
+            finish()
+            System.exit(0)
+        })
+
+
+
+
+        //import font
+        var MLight: Typeface =  Typeface.createFromAsset(assets, "fonts/MLight.ttf")
+        var MRegular: Typeface =  Typeface.createFromAsset(assets, "fonts/MRegular.ttf")
+
+        //implement font
+        tvIntroUpper.setTypeface(MRegular)
+        tvIntroLower.setTypeface(MLight)
+        btnStarted.setTypeface(MRegular)
+
+
     }
 
-    fun updateTime() {
-        dateView.setText(getTime())
+    override fun onBackPressed() {
+        Toast.makeText(this,"Back button can\'t be used", Toast.LENGTH_SHORT).show()
+        Log.i("MainActivity","Back is Pressed")
     }
 
-    private val FORMAT: SimpleDateFormat = SimpleDateFormat("'Its' HH:mm:ss, EEEE, d MMM yyyy")
 
-    fun getTime(): String? {
-        return FORMAT.format(Calendar.getInstance().getTime()).toString()
-    }
-
-    fun getNama(): CharSequence? {
-        return getText(R.string.nama)
-    }
 }
---
+++ Lab1/app/src/main/java/id/ac/ui/cs/mobileprogramming/usamahnashirulhaq/helloworld/StopWatchAct.kt
@@ -0,0 +1,98 @@
+package id.ac.ui.cs.mobileprogramming.usamahnashirulhaq.helloworld
+
+import android.content.Intent
+import android.graphics.Typeface
+import android.os.Bundle
+import android.os.SystemClock
+import android.util.Log
+import android.view.View
+import android.view.animation.Animation
+import android.view.animation.AnimationUtils
+import android.widget.*
+import androidx.appcompat.app.AppCompatActivity
+import androidx.databinding.DataBindingUtil
+import id.ac.ui.mobileprogramming.usamahnashirulhaq.helloworld.R
+import id.ac.ui.mobileprogramming.usamahnashirulhaq.helloworld.databinding.ActivityStopwatchAppBinding
+
+
+class StopWatchAct : AppCompatActivity() {
+    private lateinit var binding: ActivityStopwatchAppBinding
+    private lateinit var ivAnchor: ImageView
+    private lateinit var btnStart: Button
+    private lateinit var btnStop: Button
+    private lateinit var btnExit: ImageButton
+    private lateinit var roundingAnim: Animation
+    private lateinit var btnAnim: Animation
+    private lateinit var chronometer: Chronometer
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        Log.i("StopWatchAct", "StopWatch Created")
+        binding = DataBindingUtil.setContentView(this, R.layout.activity_stopwatch_app)
+
+        ivAnchor = binding.ivAnchor
+        btnStart = binding.btnStarted
+        btnStop = binding.btnStop
+        chronometer = binding.chronometer
+        btnExit = binding.btnexit
+
+        //load animations
+        roundingAnim = AnimationUtils.loadAnimation(this, R.anim.rounding_anim)
+        btnAnim = AnimationUtils.loadAnimation(this, R.anim.btn_time_anim)
+
+        //import font
+        var MRegular: Typeface =  Typeface.createFromAsset(assets, "fonts/MRegular.ttf")
+
+        //implement font
+        btnStart.setTypeface(MRegular)
+        btnStop.setTypeface(MRegular)
+
+        //implement animation
+        btnStart.startAnimation(btnAnim)
+        btnStop.alpha = 0F
+
+
+
+        btnStart.setOnClickListener(View.OnClickListener {
+            //implement animation
+            btnStart.alpha = 0F
+            btnStart.isEnabled = false
+            ivAnchor.startAnimation(roundingAnim)
+            btnStop.alpha = 1F
+            btnStop.startAnimation(btnAnim)
+
+
+            //start time
+            chronometer.base = SystemClock.elapsedRealtime()
+            chronometer.start()
+        })
+
+        btnStop.setOnClickListener(View.OnClickListener {
+            //implement animation
+            btnStart.alpha = 1F
+            btnStart.isEnabled = true
+            ivAnchor.clearAnimation()
+            btnStop.alpha = 0F
+            btnStart.startAnimation(btnAnim)
+
+
+            //start time
+            chronometer.base = SystemClock.elapsedRealtime()
+            chronometer.stop()
+        })
+
+
+        btnExit.setOnClickListener(View.OnClickListener {
+            Log.i("StopWatchAct", "Exit Button Clicked")
+            val intent = Intent(this, MainActivity::class.java)
+            intent.flags = Intent.FLAG_ACTIVITY_CLEAR_TOP
+            intent.putExtra("EXIT", true)
+            startActivity(intent)
+        })
+    }
+
+    override fun onBackPressed() {
+        Toast.makeText(this, "Back button can\'t be used", Toast.LENGTH_SHORT).show()
+        Log.i("MainActivity", "Back is Pressed")
+    }
+}
