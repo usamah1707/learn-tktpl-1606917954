3802e36b19875f4eed6497ed2c29dcdc4d1f89c8
Usamah Nashirul Haq <usamah.nashirul@ui.ac.id>
2020-12-13 13:11:11 +0700

    push native app clone from flutter

--- Lab1/app/src/main/java/id/ac/ui/cs/mobileprogramming/usamahnashirulhaq/helloworld/MainActivity.kt
+++ Lab1/app/src/main/java/id/ac/ui/cs/mobileprogramming/usamahnashirulhaq/helloworld/MainActivity.kt
@@ -2,11 +2,21 @@ package id.ac.ui.cs.mobileprogramming.usamahnashirulhaq.helloworld
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.widget.TextView
 import id.ac.ui.mobileprogramming.usamahnashirulhaq.helloworld.R
+import kotlinx.android.synthetic.main.activity_main.*
 
 class MainActivity : AppCompatActivity() {
+    var ctr = 0
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        var counter: TextView = counter
+
+        floatingActionButton.setOnClickListener {
+            ctr++
+            counter.text = ctr.toString()
+        }
     }
 }
